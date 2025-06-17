89478da553a8e13c9b6c17cc9805e075a8c2f43e
Ayesha_Nazar <ayesha.nazar@plcgroup.com>
2025-06-18 00:01:11 +0500

    basic structure

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, status;
+from decouple import config;
+from supabase import create_client,Client;
+import random
+
+url=config("SUPABASE_URL")
+key=config("SUPABASE_KEY")
+
+
+app=FastAPI()
+supabase : Client =create_client(url,key)
+id=random.randint(0,100)
