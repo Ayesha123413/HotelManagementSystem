5ceb2623aca7af8f1feb257d9c68081e49de2a98
Ayesha_Nazar <ayesha.nazar@plcgroup.com>
2025-06-18 22:23:16 +0500

    hash password

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI, status;
+from fastapi import FastAPI, status,HTTPException;
 from decouple import config;
 from supabase import create_client,Client;
 import random
-
+from Schemas.User import UserSignup,SignupResponse
+from passlib.context import CryptContext
 url=config("SUPABASE_URL")
 key=config("SUPABASE_KEY")
 jwt=config("SUPABASE_JWT_SECRET")
 
 app=FastAPI()
 supabase : Client =create_client(url,key)
-id=random.randint(0,100)
+
+pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+@app.post("/signup",status_code=status.HTTP_201_CREATED,tags=["Authentication"])
+async def signup_user(user_data:UserSignup):
+    existing_user=supabase.table("users").select("*").eq("email",user_data.email).execute()
+    if existing_user.data:
+        raise HTTPException(status_code=400,detail="user already existed with this email")
+    result=supabase.table("user").insert({
+        "name":user_data.name,
+        "email":user_data.email,
+        "password":pwd_context.hash(user_data.password)
+    }).execute()
+
+    if not result.data:
+        raise HTTPException(status_code=500,detail="failed to create user")
+    created_user=result.data[0]
+    return SignupResponse(
+        message="user created succesfully",
+        user_id=created_user["id"],
+        email=created_user["email"]
+   )
