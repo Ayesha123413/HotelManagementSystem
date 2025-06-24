4e5786f58a3f177fea9651bb4eb43519133d3bfe
Ayesha_Nazar <ayesha.nazar@plcgroup.com>
2025-06-24 23:54:40 +0500

    room :: delete and update query

--- Routers/room.py
+++ Routers/room.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI,APIRouter,Depends,HTTPException,status
 from supabase import create_client,Client;
-from Schemas.Room import Room
+from Schemas.Room import Room,RoomUpdate
 from decouple import config
 from auth.dependencies import get_current_user
+
 
 router =APIRouter(prefix="/rooms")
 
@@ -20,5 +21,29 @@ async def create_room(room:Room, current_user=Depends(get_current_user)):
     if not result.data:
         raise HTTPException(status_code=500,detail="Failed to create Room")
     return {"message":"Room is successfully created","room":result.data[0]}
+
+
+@router.post("/delete/{room_no}",status_code=status.HTTP_200_OK)
+async def delete_room(room_no:int,current_user=Depends(get_current_user)):
+    existing=supabase.table("Rooms").select("*").eq("room_number",room_no).execute()
+    if not existing.data:
+        raise HTTPException(status_code=404, detail="room does not exist")
+    result=supabase.table("Rooms").delete().eq("room_number",room_no).execute()
+    if not result.data:
+        raise HTTPException(status_code=500,detail="failed to delete")
+    return{"message":f"Room {room_no} is succesfully deleted"}
+
+@router.put("editRoom/{room_no}",status_code=status.HTTP_200_OK)
+async def edit_room(
+    room_no:int,
+    room_data:RoomUpdate,
+    current_user=Depends(get_current_user)):
+    existing=supabase.table("Rooms").select("*").eq("room_number",room_no).execute()
+    if not existing.data:
+         raise HTTPException(status_code=404, detail="room does not exist")
+    result=supabase.table("Rooms").update(room_data.dict()).eq("room_number",room_no).execute()
+    if not result.data:
+        raise HTTPException(status_code=500,detail="failed to update")
+    return{"message":f"Room {room_no} is succesfully updated"}
 
 
--- Schemas/Room.py
+++ Schemas/Room.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class Room(BaseModel):
     room_number:int
     room_type:str
     room_status:bool
     price:int
+class RoomUpdate(BaseModel):
+    room_type: Optional[str] = None
+    room_status: Optional[bool] = None
+    price: Optional[int] = None
